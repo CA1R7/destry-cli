888e624e939319cb02fb792aeb862b56936fc63c
CA1R7 <cairzz222@gmail.com>
2021-09-12 14:27:52 +0100

    Removed install file and merged it into start file which will detect automaticlly if `node_modules` does not exist

--- shells/start_application.bat
+++ shells/start_application.bat
@@ -1,9 +1,13 @@
 @echo off
 node -v 2 > Nul
 if "%errorlevel%" == "9009" (
-  echo node could not found
+  echo node.exe is required please install it from https://nodejs.org/en/download then try again
 ) else (
   cd ..
+  IF NOT EXIST node_modules/ (
+    echo Installing all the dependencies required ...
+    npm install
+  )
   IF NOT EXIST dist/ (
     echo Building resources ...
     npm run build
@@ -11,4 +15,3 @@ if "%errorlevel%" == "9009" (
   echo Starting application ...
   npm run start
 )
-pause
