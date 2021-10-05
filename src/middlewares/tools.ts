bc095dbafac222d9ad0ff95ed70887f468f2b1ba
CA1R7 <cairzz222@gmail.com>
2021-10-05 20:55:41 +0100

    Fixed token generator also added file handler

--- src/middlewares/tools.ts
+++ src/middlewares/tools.ts
@@ -1,6 +1,7 @@
-import { writeFileSync } from "fs";
 import { join } from "path";
 import request from "request";
+import { FileHandler } from "../utils/file_handler";
+import { Logger } from "../utils/logger";
 import { OptionsType } from "../utils/preview";
 import { ProxiesHandler } from "../utils/proxies_handler";
 
@@ -12,17 +13,18 @@ interface ToolManagerType {
 export const ToolStartManager = ({ options, proxieshandler }: ToolManagerType): void => {
   const started_stamp: number = Date.now();
   let proxy: string, token: string, ipr: number;
-  let i: number = (ipr = 0);
+  let i: number = (ipr = 0x0);
 
   const requestLoop = setInterval(() => {
     try {
       if (options.max_checks && options.max_checks <= i) {
         clearInterval(requestLoop);
-        console.log(`Checking Done ${Date.now() - started_stamp}ms`);
+        Logger.info(`Checking Done ${Date.now() - started_stamp}ms`);
         return;
       }
-      token = g_token(Math.random() >= 0.5 ? 1 : 2);
-      proxy = proxieshandler.proxies[ipr >= proxieshandler.proxies.length - 1 ? (ipr = 0) : ipr];
+      token = g_token(Math.random() >= 0.5 ? 0x2 : 0x1);
+      proxy =
+        proxieshandler.proxies[ipr >= proxieshandler.proxies.length - 0x1 ? (ipr = 0x0) : ipr];
 
       if (!proxy) {
         throw new Error(`[${proxy}] Invalid proxy line`);
@@ -49,34 +51,43 @@ export const ToolStartManager = ({ options, proxieshandler }: ToolManagerType): 
               throw new Error("Invalid token generated");
             }
             const l = JSON.parse(body);
-            if (l.code !== 0) {
-              console.log(`[\x1b[32m+\x1b[0m] valid token [${token}]`);
-              writeFileSync(join(__dirname, `../../result/token-${Date.now()}.txt`), `token -> ${token}`);
+            if (l.code !== 0x0) {
+              console.log(l);
+              Logger.info(`valid token [${token}]`);
+              FileHandler.writeFile<string>(
+                join(__dirname, `../../result/token-${Date.now()}.txt`),
+                `token -> ${token}`,
+              );
             } else throw new Error("Invalid token generated");
           } catch (e) {
-            console.log(`[\x1b[31m+\x1b[0m] Invalid token [\x1b[35m${proxy}\x1b[0m][${token.slice(0, 30)}..]`);
+            Logger.error(
+              `Invalid token ${
+                options.proxiesQs === "yes" ? `[\x1b[35m${proxy}\x1b[0m]` : ""
+              }[${token.slice(0x0, 0x1e)}..]`,
+            );
           }
         },
       );
       ipr++;
       i++;
     } catch (e) {
-      console.log(String(e));
+      Logger.error(String(e));
     }
   }, options.delay_time * 1e3);
 };
 
-export const g_token = (version_token = 1): string => {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz123456789_-";
-  const gn = (_length: number) => {
-    const p = [];
-    for (let i = 0; i < _length; i++) {
+export const g_token = (version_token = 0x1): string => {
+  const gn = (_length: number, symbols = false) => {
+    const characters: string =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz123456789" + (symbols ? "_-" : "");
+    const p: string[] = [];
+    for (let i = 0x0; i < _length; i++) {
       p.push(characters.charAt(~~(Math.random() * characters.length)));
     }
     return p.join("");
   };
-  if (version_token === 2) {
-    return `m${gn(2)}.${gn(84)}`;
+  if (version_token === 0x2) {
+    return `m${gn(0x2)}.${gn(0x54, true)}`;
   }
-  return `N${gn(23)}.${gn(6)}.${gn(27)}`;
+  return `ND${gn(0x16)}.${gn(0x6)}.${gn(0x1b, true)}`;
 };
